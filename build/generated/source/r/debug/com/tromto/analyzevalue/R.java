86a2e2b0aec2edbbfb16b07563f52303625d9ffa
tromso <ktromso@gmail.com>
2014-11-16 02:50:53 +0000

    edittext

--- build/generated/source/r/debug/com/tromto/analyzevalue/R.java
+++ build/generated/source/r/debug/com/tromto/analyzevalue/R.java
@@ -10,66 +10,80 @@ package com.tromto.analyzevalue;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f040000;
+        public static final int blue=0x7f040001;
+        public static final int dirty=0x7f040002;
+        public static final int green=0x7f040003;
+        public static final int red=0x7f040004;
+        public static final int white=0x7f040005;
+        public static final int white2=0x7f040006;
+    }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
-        public static final int navigation_drawer_width=0x7f040002;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int navigation_drawer_width=0x7f050002;
     }
     public static final class drawable {
-        public static final int drawer_shadow=0x7f020000;
-        public static final int ic_drawer=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int background1=0x7f020000;
+        public static final int button_custom=0x7f020001;
+        public static final int button_text_color=0x7f020002;
+        public static final int drawer_shadow=0x7f020003;
+        public static final int edit_text_design=0x7f020004;
+        public static final int ic_drawer=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f080003;
-        public static final int action_example=0x7f080030;
-        public static final int action_settings=0x7f08002f;
-        public static final int button1=0x7f08002e;
-        public static final int container=0x7f080001;
-        public static final int drawer_layout=0x7f080000;
-        public static final int editText1=0x7f08000a;
-        public static final int editText10=0x7f08001b;
-        public static final int editText11=0x7f080022;
-        public static final int editText12=0x7f080023;
-        public static final int editText13=0x7f08002a;
-        public static final int editText14=0x7f08002b;
-        public static final int editText15=0x7f08002c;
-        public static final int editText16=0x7f08002d;
-        public static final int editText2=0x7f08000b;
-        public static final int editText3=0x7f08000e;
-        public static final int editText4=0x7f08000f;
-        public static final int editText5=0x7f080012;
-        public static final int editText6=0x7f080013;
-        public static final int editText7=0x7f080016;
-        public static final int editText8=0x7f080017;
-        public static final int editText9=0x7f08001a;
-        public static final int navigation_drawer=0x7f080002;
-        public static final int tableRow1=0x7f080004;
-        public static final int tableRow10=0x7f080029;
-        public static final int tableRow2=0x7f080008;
-        public static final int tableRow3=0x7f08000c;
-        public static final int tableRow4=0x7f080010;
-        public static final int tableRow5=0x7f080014;
-        public static final int tableRow6=0x7f080018;
-        public static final int tableRow7=0x7f08001c;
-        public static final int tableRow8=0x7f080020;
-        public static final int tableRow9=0x7f080024;
-        public static final int textView1=0x7f080005;
-        public static final int textView10=0x7f08001e;
-        public static final int textView11=0x7f08001f;
-        public static final int textView12=0x7f080021;
-        public static final int textView13=0x7f080025;
-        public static final int textView14=0x7f080026;
-        public static final int textView15=0x7f080027;
-        public static final int textView16=0x7f080028;
-        public static final int textView2=0x7f080006;
-        public static final int textView3=0x7f080007;
-        public static final int textView4=0x7f080009;
-        public static final int textView5=0x7f08000d;
-        public static final int textView6=0x7f080011;
-        public static final int textView7=0x7f080015;
-        public static final int textView8=0x7f080019;
-        public static final int textView9=0x7f08001d;
+        public static final int LinearLayout1=0x7f090004;
+        public static final int action_example=0x7f090031;
+        public static final int action_settings=0x7f090030;
+        public static final int button1=0x7f09002f;
+        public static final int container=0x7f090001;
+        public static final int drawer_layout=0x7f090000;
+        public static final int editText1=0x7f09000b;
+        public static final int editText10=0x7f09001c;
+        public static final int editText11=0x7f090023;
+        public static final int editText12=0x7f090024;
+        public static final int editText13=0x7f09002b;
+        public static final int editText14=0x7f09002c;
+        public static final int editText15=0x7f09002d;
+        public static final int editText16=0x7f09002e;
+        public static final int editText2=0x7f09000c;
+        public static final int editText3=0x7f09000f;
+        public static final int editText4=0x7f090010;
+        public static final int editText5=0x7f090013;
+        public static final int editText6=0x7f090014;
+        public static final int editText7=0x7f090017;
+        public static final int editText8=0x7f090018;
+        public static final int editText9=0x7f09001b;
+        public static final int navigation_drawer=0x7f090002;
+        public static final int tableRow1=0x7f090005;
+        public static final int tableRow10=0x7f09002a;
+        public static final int tableRow2=0x7f090009;
+        public static final int tableRow3=0x7f09000d;
+        public static final int tableRow4=0x7f090011;
+        public static final int tableRow5=0x7f090015;
+        public static final int tableRow6=0x7f090019;
+        public static final int tableRow7=0x7f09001d;
+        public static final int tableRow8=0x7f090021;
+        public static final int tableRow9=0x7f090025;
+        public static final int textView=0x7f090003;
+        public static final int textView1=0x7f090006;
+        public static final int textView10=0x7f09001f;
+        public static final int textView11=0x7f090020;
+        public static final int textView12=0x7f090022;
+        public static final int textView13=0x7f090026;
+        public static final int textView14=0x7f090027;
+        public static final int textView15=0x7f090028;
+        public static final int textView16=0x7f090029;
+        public static final int textView2=0x7f090007;
+        public static final int textView3=0x7f090008;
+        public static final int textView4=0x7f09000a;
+        public static final int textView5=0x7f09000e;
+        public static final int textView6=0x7f090012;
+        public static final int textView7=0x7f090016;
+        public static final int textView8=0x7f09001a;
+        public static final int textView9=0x7f09001e;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -83,22 +97,22 @@ public final class R {
         public static final int fragment_navigation_drawer=0x7f030008;
     }
     public static final class menu {
-        public static final int global=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int global=0x7f080000;
+        public static final int main=0x7f080001;
     }
     public static final class string {
-        public static final int action_example=0x7f050000;
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050002;
-        public static final int navigation_drawer_close=0x7f050003;
-        public static final int navigation_drawer_open=0x7f050004;
-        public static final int title_section1=0x7f050005;
-        public static final int title_section2=0x7f050006;
-        public static final int title_section3=0x7f050007;
-        public static final int title_section4=0x7f050008;
-        public static final int title_section5=0x7f050009;
-        public static final int title_section6=0x7f05000a;
-        public static final int title_section7=0x7f05000b;
+        public static final int action_example=0x7f060000;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060002;
+        public static final int navigation_drawer_close=0x7f060003;
+        public static final int navigation_drawer_open=0x7f060004;
+        public static final int title_section1=0x7f060005;
+        public static final int title_section2=0x7f060006;
+        public static final int title_section3=0x7f060007;
+        public static final int title_section4=0x7f060008;
+        public static final int title_section5=0x7f060009;
+        public static final int title_section6=0x7f06000a;
+        public static final int title_section7=0x7f06000b;
     }
     public static final class style {
         /** 
@@ -109,9 +123,9 @@ public final class R {
  API 11 theme customizations can go here. 
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
